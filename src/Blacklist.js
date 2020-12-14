45a5d542aa31db74d611c6ab7949ecb9e14cc224
Derpinou <lightcompte@gmail.com>
2020-12-14 12:03:47 +0100

    fix blacklist

--- src/Blacklist.js
+++ src/Blacklist.js
@@ -4,11 +4,14 @@ class BlackList {
         this.token = token;
     }
     async checkBlackList (id) {
+        return console.log("You can't use this function, api balckist are not updated for work")
+        /*
         fetch(`https://protect-bot.fr/api/?apiKey=${this.token}&checkblacklist=${id}`)
             .then(response => response.json())
             .then(data => {
                 console.log(data)
             })
+            */
     }
 }
 module.exports = BlackList
