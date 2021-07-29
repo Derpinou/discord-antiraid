777e1b40155ca4a2fdf6ab420175573dd2bb1ed4
Derpinou <lightcompte@gmail.com>
2021-07-29 10:14:24 +0200

    Create Example with quickdb

---
+++ example/AntiRaid/quickdb.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js'),
+    {AntiRaid} = require('discord-antiraid'),
+    client = new Discord.Client(),
+    db = require('quick.db');
+
+
+//Extend AntiRaid class for edit save(id: String, cooldown: []) and getOptionsFromDB(id: String) with your db methods
+class AntiRaidWithDB extends AntiRaid {
+    //AntiRaid.save is Optional
+    async save(id, cooldown) {
+        db.set(`cooldown_${id}`, cooldown)
+    }
+    //If the bot is public on several guilds and each guilds must have its own antiraid configuration
+    async getOptionsFromDB(id) {
+        return db.get(`antiraid_${id}`)
+    }
+}
+const antiraid = new AntiRaidWithDB(client, {
+    rateLimit: 3,
+    time: 10000,
+    ban: true,
+    kick: false,
+    unrank: false,
+    exemptMembers: [],
+    exemptRoles: [],
+    exemptEvent: [],
+    reason: "discord-antiraid"
+})
+
+antiraid.on("punish", (member, reason, sanction) => {
+    member.guild.channels.cache.get(db.get(`logs_${member.guild.id}`)).send(`${member.user.username} got banned for raid attempt`)
+})
