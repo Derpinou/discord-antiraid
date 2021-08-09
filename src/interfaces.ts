f8ffd92642d7d07dadea72f70fe1c711a61b1227
Derpinou <lightcompte@gmail.com>
2021-08-09 14:38:31 +0200

    Create interfaces.ts

---
+++ src/interfaces.ts
@@ -0,0 +1,35 @@
+import {AntiRaid} from "./Antiraid";
+
+export interface AntiRaidOptions {
+    rateLimit: number
+    time: number
+    ban: boolean
+    kick: boolean
+    unrank: boolean
+    exemptMembers: Array<string>
+    exemptRoles: Array<string>
+    exemptEvent: Array<string>
+    reason: string
+}
+
+export interface Cooldown {
+    id?: string;
+    guild?: string;
+    event?: string;
+    startedAt?: number;
+    rate?: number;
+}
+
+interface ActionTypeObject {
+    id: number;
+    name: string;
+}
+type djsEvents = 'channelCreate'|'channelDelete'|'guildBanAdd'|'guildMemberRemove'|'roleCreate'|'roleDelete';
+export type ActionType = {
+    [key in djsEvents]: ActionTypeObject;
+};
+
+export interface AntiRaidEvent {
+    name: string;
+    run: (client: AntiRaid, ...args: any[]) => Promise<void>;
+}
